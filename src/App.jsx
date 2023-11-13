c554cd9152f72cea50fdedf819189517bdc73786
bathini79 <bathininipun79@gmail.com>
2023-11-14 00:38:49 +0530

    feat: appwrite configuration

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
     <h1>Hello There</h1>
