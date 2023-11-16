4e8aae87a3a6885a1b0180b7e8d3c0dba8b05373
bathini79 <bathininipun79@gmail.com>
2023-11-16 10:39:50 +0530

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+import { Header ,Footer} from './components'
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>{
+      setLoading(false)
+    })
+  },[])
   return (
     <>
-    <h1>Hello There</h1>
+    {!loading ? (<div className='min-h-sc flex flex-wrap content-between bg-grey-400'>
+        <div className='w-full block'>
+         <Header/>
+         <main></main>
+         <Footer/>
+        </div>
+    </div>) :(null)}
     </>
   )
 }
