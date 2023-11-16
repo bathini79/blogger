8561f78e93e7b393b9869233349ce81861cee984
bathini79 <bathininipun79@gmail.com>
2023-11-16 12:34:50 +0530

    Components initialize

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import authService from './appwrite/auth'
-import { login,logout } from './store/authSlice'
-import { Header ,Footer} from './components'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
 function App() {
-  const [loading,setLoading] = useState(true)
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    authService.getCurrentUser()
-    .then((userData)=>{
-      if(userData){
-        dispatch(login({userData}))
-      }
-      else{
-        dispatch(logout())
-      }
-    })
-    .finally(()=>{
-      setLoading(false)
-    })
-  },[])
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
   return (
     <>
-    {!loading ? (<div className='min-h-sc flex flex-wrap content-between bg-grey-400'>
-        <div className='w-full block'>
-         <Header/>
-         <main></main>
-         <Footer/>
+      {!loading ? (
+        <div className="min-h-sc flex flex-wrap content-between bg-grey-400">
+          <div className="w-full block">
+            <Header />
+            <main></main>
+            <Footer />
+          </div>
         </div>
-    </div>) :(null)}
+      ) : null}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Container from "./Container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./Header/Logout";
 
-export { Footer, Header };
+export { Footer, Header , Container,Logo,LogoutBtn };
